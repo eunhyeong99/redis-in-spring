e886be5d8556eab552139520c375a1f54ce515db
eunhyeong99 <132648987+eunhyeong99@users.noreply.github.com>
2024-10-20 14:46:13 +0900

    Revert "first commit"
    This reverts commit c06061b76dc5cc2111e5ba5e378bee31d18b48aa.

--- src/main/java/com/sparta/redisinspring/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.redisinspring;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-    Page<Board> findAllByOrderByCreatedAtDesc(Pageable pageable);
-}
